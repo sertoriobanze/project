43d5b5e3468bf56abde3af2f8d16b965ab70739e
sertoriobanze <111365373+sertoriobanze@users.noreply.github.com>
2022-10-24 05:29:22 -0700

    first commit

---
+++ Real-Brilho/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public_css/home_page_style.css">
+    <link rel="shortcut icon" href="./public_img/icons/logo_on.png" type="image/x-icon">
+    <title>Home Page</title>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <h1><img src="./public_img/icons/logo.png" class="logo_img" alt="logo-campany"></h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#" class="gg">Servicos</a>
+                <div class="subnav">
+                    <ul>
+                        <li><a href="#">Pintura</a></li>
+                        <li><a href="#">Montagem</a></li>
+                        <li><a href="#">Venda</a></li>
+                        
+                    </ul>
+                </div>
+        
+            </li>
+            <li><a href="#">Feed</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><button class="btn" id="contact_btn">Contact</button></li>
+            
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section class="first">
+        <div class="design">
+            
+        </div>
+        <div class="first__content">
+            <h1>MODERNISE A SUA CASA</h1>
+            <p>Nos ajudamos a modernizar e a deixar sua casa mais elegante, com ajuda de proficionais qualificados.</p>
+
+        </div>
+        <div class="first__img">
+
+        <h1>IMAGEM</h1>
+        </div>
+    </section>
+</main>
+    
+
+</body>
+</html>
